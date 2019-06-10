7187f66cebfb92f14c07b52f8c1cb8c5f0d7d82e
bhanu velamuri <35382776+bhanuvelamuri@users.noreply.github.com>
2019-06-10 16:43:14 -0400

    TestNG - data providers using arrays

--- TestNG/src/Selenium/GmailData.java
+++ TestNG/src/Selenium/GmailData.java
@@ -20,8 +20,12 @@ public class GmailData {
 	public static String[][] credentials() {
 		return new String[][] {{"bpr1406.v@gmail.com","mercury1"},{"bpr1406.bp@gmail.com","mercury"},{"bpr1406.v@gmail.com","mercury1"},{"bhanu.velamuri@gmail.com","mercury"}};
 }
+	@DataProvider(name="webapp")
+	public static String[] weblinks() {
+		return new String[] {"http://exciteservices.net","http://linkedin.com","http://github.com","http://gmail.com"};
+}
 	@Test(dataProvider = "Auth")
-	public void testData(String sUser, String sPass) {
+	public void testData1(String sUser, String sPass) {
 		System.setProperty(constrants.chromeKey, constrants.chromeDriverPath);
 		driver = new ChromeDriver();
 		driver.manage().window().maximize(); 
@@ -33,6 +37,16 @@ public class GmailData {
 		WebDriverWait wait = new WebDriverWait(driver, 10);
 		WebElement el = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[contains(text(),'Next')]")));
 		el.click();
+		System.out.println(driver.getTitle());
+		driver.close();
+	}
+	@Test(dataProvider = "webapp")
+	public void testData2(String link) {
+		System.setProperty(constrants.chromeKey, constrants.chromeDriverPath);
+		driver = new ChromeDriver();
+		driver.manage().window().maximize(); 
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.get(link);
 		System.out.println(driver.getTitle());
 		driver.close();
 	}
