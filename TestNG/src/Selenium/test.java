9f76d74553d7344119d8c933ce065976ab52b39f
bhanu velamuri <35382776+bhanuvelamuri@users.noreply.github.com>
2019-06-11 14:20:40 -0400

    TestNG - Exercise - test

--- TestNG/src/Selenium/test.java
+++ TestNG/src/Selenium/test.java
@@ -1,14 +1,22 @@
 package Selenium;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import SeleniumConstreants.constrants;
@@ -59,4 +67,74 @@ public class test {
 	public void Printing() {
 		System.out.println("Test Sucessfull");
 	}
+	
+	
+	/*public WebDriver driver;
+	public String baseUrl = "http://newtours.demoaut.com";
+	@BeforeSuite
+	public void runningTest() {
+		System.out.println( "+---------------------------------------------+");
+		System.out.println( "|        opening launcher                     |");
+		System.out.println( "+---------------------------------------------+");
+		System.setProperty(constrants.chromeKey, constrants.chromeDriverPath);
+		driver = new ChromeDriver();
+	}
+	
+	@BeforeTest
+	public void launcher() {  
+		driver.manage().window().maximize(); 
+		System.out.println( "+---------------------------------------------+");
+		System.out.println( "|        maximizing window                    |");
+		System.out.println( "+---------------------------------------------+");
+		driver.get(baseUrl);
+		System.out.println("|        opening url                          |");
+		System.out.println( "+---------------------------------------------+");
+		
+	}
+	
+	@BeforeClass
+	public void url() {
+		System.out.println( "|        Stating Test                         |");
+		System.out.println( "+---------------------------------------------+");
+	}
+	
+	@Test
+	public void login() throws InterruptedException {
+		// TODO Auto-generated method stub
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.findElement(By.xpath("//input[@name='userName']")).sendKeys("mercury");
+		System.out.println("Entering Username");
+		driver.findElement(By.xpath("//input[@name='password']")).sendKeys("mercury");
+		System.out.println("Entering Password");
+		driver.findElement(By.xpath("//input[@value='Login']")).click();
+		System.out.println("clicking on submit");
+		String expetedurl = "http://newtours.demoaut.com/mercuryreservation.php";
+		String actualurl = driver.getCurrentUrl();
+		if(actualurl.contains(expetedurl)) {
+			System.out.println("login successfull");
+		}
+		else {
+			System.out.println("login failed");
+		}
+		
+		driver.navigate().back();
+	}
+	@AfterClass
+	public void closetab() {
+		System.out.println(driver.getTitle());
+		Assert.assertEquals("Welcome: Mercury Tours", driver.getTitle());
+		System.out.println("Title Printed");
+	}
+	
+	@AfterTest
+	public void close() {
+		driver.findElement(By.xpath("//a[contains(text(),'SUPPORT')]")).click();
+		System.out.println(driver.getPageSource());
+		System.out.println( "+----------------------------------------------");
+	}
+	@AfterSuite
+	public void closeTest() {
+		driver.close();
+		System.out.println("Test Completed");
+	}*/
 }
