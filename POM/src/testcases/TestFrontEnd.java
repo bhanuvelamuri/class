d3443d16431b447de9a096f5013f9393d9b0ca7e
bhanu velamuri <35382776+bhanuvelamuri@users.noreply.github.com>
2019-06-17 17:24:02 -0400

    update

--- POM/src/testcases/TestFrontEnd.java
+++ POM/src/testcases/TestFrontEnd.java
@@ -1,23 +1,36 @@
 package testcases;
 
-import org.openqa.selenium.WebDriver;
-import org.testng.annotations.BeforeMethod;
+import static org.testng.Assert.assertTrue;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import utilities.constrants;
 import webpages.FrontEndLogin;
 
-public class TestFrontEnd {
+public class TestFrontEnd  {
 	WebDriver driver;
 	FrontEndLogin frontLogin;
 	@BeforeMethod
-	public void login() {
-		By user = 
-		By pass = 
-		By remember = 
-		By click = 
+	public void setup() {
+		System.setProperty(constrants.chromeKey, constrants.chromeDriverPath);
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 	}
-	public void FrontEndLogin(driver) {
-			this.driver = driver;
-		}
-	
+	@Test
+	public void login() {
+		driver.get("https://www.phptravels.net/admin-portal/admin");
+		frontLogin = new FrontEndLogin(driver);
+		boolean loginstatus;
+		loginstatus = frontLogin.loginFrontEnd("admin@phptravels.com","demoadmin");
+		assertTrue(loginstatus);
+	}
+
 
 }
--- POM/src/webpages/FrontEndLogin.java
+++ POM/src/webpages/FrontEndLogin.java
@@ -1,6 +1,33 @@
 package webpages;
 
-public class FrontEndLogin {
-	
+import org.openqa.selenium.WebDriver;
 
-}
+import org.openqa.selenium.By;
+
+public class FrontEndLogin {
+	WebDriver driver;
+			By user = By.xpath("//input[@placeholder='Email']");
+			By pass = By.xpath("//input[@placeholder='Password']");
+			By remember = By.xpath("//*[@class='iCheck-helper']");
+			By click = By.xpath("//button[@type='submit']");
+		public FrontEndLogin(WebDriver driver) {
+			// TODO Auto-generated constructor stub
+			this.driver = driver;
+		}
+
+
+		public boolean loginFrontEnd(String username, String password) {
+			// TODO Auto-generated method stub
+			driver.findElement(user).sendKeys(username);
+			driver.findElement(pass).sendKeys(password);
+			driver.findElement(remember).click();
+			driver.findElement(click).click();
+			String expectedUrlUrl = "https://www.phptravels.net/admin-portal/";
+			String currentUrl = driver.getCurrentUrl();
+			if(driver.getCurrentUrl().contains(expectedUrlUrl)) 
+				return true;
+			else
+				return false;
+			
+		}
+		}
