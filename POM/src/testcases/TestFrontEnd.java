14e69f4bbd77205bc26b1e95b390b719bcf99680
bhanu velamuri <35382776+bhanuvelamuri@users.noreply.github.com>
2019-06-17 16:39:13 -0400

    POM

---
+++ POM/src/testcases/TestFrontEnd.java
@@ -0,0 +1,23 @@
+package testcases;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeMethod;
+
+import webpages.FrontEndLogin;
+
+public class TestFrontEnd {
+	WebDriver driver;
+	FrontEndLogin frontLogin;
+	@BeforeMethod
+	public void login() {
+		By user = 
+		By pass = 
+		By remember = 
+		By click = 
+	}
+	public void FrontEndLogin(driver) {
+			this.driver = driver;
+		}
+	
+
+}
---
+++ POM/src/utilities/constrants.java
@@ -0,0 +1,14 @@
+package utilities;
+
+public class constrants {
+
+	public static final String chromeKey = "webdriver.chrome.driver";
+	public static final String chromeDriverPath = "C:\\bhanu\\chromedriver.exe";
+	public static final String geckoKey = "webdriver.gecko.driver";
+	public static final String geckoDriverPath = "C:\\bhanu\\geckodriver.exe";
+	public static final String internetExplorer = "webdriver.ie.driver";
+	public static final String IEDriverPath = "C:\\bhanu\\IEDriverServer.exe";
+	public static final String URL = "http://google.com";
+	public static final String NEWTOURSURL = "http://newtours.demoaut.com";
+
+}
